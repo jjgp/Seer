d6548b0ab451fc9c1ba2d67d57d0054a45d29c78
Jason Prasad <jasongprasad@gmail.com>
2018-07-08 20:58:05 -0400

    WIP using lexer

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,12 +1,20 @@
+import org.antlr.v4.runtime.CharStreams
+import java.io.FileInputStream
+
 class App {
-    val greeting: String
-        get() = "Hello world."
 
     companion object {
 
         @JvmStatic
         fun main(args: Array<String>) {
-            println(App().greeting)
+            val inputStream = if (!args.isNotEmpty()) {
+                System.`in`
+            } else {
+                FileInputStream(args[0])
+            }
+
+            val charStream = CharStreams.fromStream(inputStream)
+            JavaScriptLexer(charStream)
         }
     }
 }
